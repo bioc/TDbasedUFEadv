7d7f2a631e18b39cabb33a6f6bf85f92c68bcafc
tagtag <tag@granular.com>
2023-01-29 17:57:14 +0900

    Repalce NA with zero in prepareTensorfromMatrix

--- R/prepareTensorfromMatrix.R
+++ R/prepareTensorfromMatrix.R
@@ -11,6 +11,8 @@
 prepareTensorfromMatrix <-function(matrix1,matrix2){
     L1 <- dim(matrix1)[2]
     L2 <- dim(matrix2)[2]
+    matrix1[is.na(matrix1)] <-0 
+    matrix1[is.na(matrix2)] <-0 
     Z <- apply(cbind(matrix1,matrix2),1,function(x){outer(x[seq_len(L1)],x[L1+seq_len(L2)])})
     dim(Z) <- c(L1,L2,dim(Z)[2])
     Z <- aperm(Z,c(3,1,2))
