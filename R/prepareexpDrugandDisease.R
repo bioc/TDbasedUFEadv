ad8c63fb4814f15a9252bad5e0b4474e14134813
tagtag <tag@granular.com>
2023-03-12 16:34:32 +0900

    v-0.99.4

--- R/prepareexpDrugandDisease.R
+++ R/prepareexpDrugandDisease.R
@@ -11,38 +11,14 @@
 #' require(RTCGA.rnaseq)
 #' LIST <- list(ACC.rnaseq,
 #'             BLCA.rnaseq,
-#'             BRCA.rnaseq,
-#'             CESC.rnaseq,
-#'             COAD.rnaseq,
-#'             ESCA.rnaseq,
-#'             GBM.rnaseq,
-#'             HNSC.rnaseq,
-#'             KICH.rnaseq,
-#'             KIRC.rnaseq,
-#'             KIRP.rnaseq,
-#'             LGG.rnaseq,
-#'             LIHC.rnaseq,
-#'             LUAD.rnaseq,
-#'             LUSC.rnaseq,
-#'             OV.rnaseq,
-#'             PAAD.rnaseq,
-#'             PCPG.rnaseq,
-#'             PRAD.rnaseq,
-#'             READ.rnaseq,
-#'             SARC.rnaseq,
-#'             SKCM.rnaseq,
-#'             STAD.rnaseq,
-#'             TGCT.rnaseq,
-#'             THCA.rnaseq,
-#'             UCEC.rnaseq,
-#'             UCS.rnaseq)
+#'             BRCA.rnaseq)
 #' dummy <- prepareexpDrugandDisease(LIST)
 #' }
 prepareexpDrugandDisease <- function(LIST){
     dir <- system.file("extdata", package="TDbasedUFEadv")
     TCGA <- read.csv(file.path(dir,"drug_response.txt"), sep="\t")
     TCGAdb <- unique(TCGA[,1])
-    TCGAdb <- TCGAdb[-16]
+    if (length(TCGAdb)>16) TCGAdb <- TCGAdb[-16]
     RTCGA_all <- matrix(NA, nrow=dim(TCGA)[1],ncol=dim(LIST[[1]])[2])
     LISTp <- rep(list(NA),length(LIST))
     toTCGA <- function(x){
@@ -52,16 +28,11 @@ prepareexpDrugandDisease <- function(LIST){
         return( x[ID %in% TCGA[,2],])
     }
     LISTp <- lapply(LIST,toTCGA)
-    expDrug <- rbind(data.frame(LISTp[[1]]),data.frame(LISTp[[2]]),
-        data.frame(LISTp[[3]]),data.frame(LISTp[[4]]),data.frame(LISTp[[5]]),
-        data.frame(LISTp[[6]]),data.frame(LISTp[[7]]),data.frame(LISTp[[8]]),
-        data.frame(LISTp[[9]]),data.frame(LISTp[[10]]),data.frame(LISTp[[11]]),
-        data.frame(LISTp[[12]]),data.frame(LISTp[[13]]),data.frame(LISTp[[14]]),
-        data.frame(LISTp[[15]]),data.frame(LISTp[[16]]),data.frame(LISTp[[17]]),
-        data.frame(LISTp[[18]]),data.frame(LISTp[[19]]),data.frame(LISTp[[20]]),
-        data.frame(LISTp[[21]]),data.frame(LISTp[[22]]),data.frame(LISTp[[23]]),
-        data.frame(LISTp[[24]]),data.frame(LISTp[[25]]),data.frame(LISTp[[26]]),
-                     data.frame(LISTp[[27]]))
+    expDrug <- NULL
+    for (i in seq_len(length(LISTp)))
+    {
+        expDrug <- rbind(expDrug,data.frame(LISTp[[i]]))
+    }
     expDrug <- convertTCGA(expDrug)
     ID <- lapply(strsplit(as.character(LIST[[3]][,1]),"-"),"[",seq_len(3))
     ID <- t(data.frame(ID))
--- tests/testthat/test-TDbasedUFadv.R
+++ tests/testthat/test-TDbasedUFadv.R
@@ -4,38 +4,14 @@ test_that("multiplication works", {
 require(RTCGA.rnaseq)
 LIST <- list(ACC.rnaseq,
              BLCA.rnaseq,
-             BRCA.rnaseq,
-             CESC.rnaseq,
-             COAD.rnaseq,
-             ESCA.rnaseq,
-             GBM.rnaseq,
-             HNSC.rnaseq,
-             KICH.rnaseq,
-             KIRC.rnaseq,
-             KIRP.rnaseq,
-             LGG.rnaseq,
-             LIHC.rnaseq,
-             LUAD.rnaseq,
-             LUSC.rnaseq,
-             OV.rnaseq,
-             PAAD.rnaseq,
-             PCPG.rnaseq,
-             PRAD.rnaseq,
-             READ.rnaseq,
-             SARC.rnaseq,
-             SKCM.rnaseq,
-             STAD.rnaseq,
-             TGCT.rnaseq,
-             THCA.rnaseq,
-             UCEC.rnaseq,
-             UCS.rnaseq)
+             BRCA.rnaseq)
 dummy <- prepareexpDrugandDisease(LIST)
 expDrug <- dummy[[1]]
 expDisease <- dummy[[2]]
 require(Biobase)
-Z <- prepareTensorfromMatrix(exprs(expDrug[seq_len(1000),seq_len(100)]),exprs(expDisease[seq_len(1000),seq_len(100)]))
-sample<- outer(colnames(expDrug)[seq_len(100)],colnames(expDisease)[seq_len(100)],function(x,y){paste(x,y)})
+Z <- prepareTensorfromMatrix(exprs(expDrug[seq_len(100),seq_len(10)]),exprs(expDisease[seq_len(100),seq_len(10)]))
+sample<- outer(colnames(expDrug)[seq_len(10)],colnames(expDisease)[seq_len(10)],function(x,y){paste(x,y)})
 require(TDbasedUFE)
-Z <- PrepareSummarizedExperimentTensor(sample=sample,feature=rownames(expDrug)[seq_len(1000)],value=Z)
+Z <- PrepareSummarizedExperimentTensor(sample=sample,feature=rownames(expDrug)[seq_len(100)],value=Z)
 HOSVD <- computeHosvd(Z)
 cond <- list(NULL,rep(1:2,each=50),rep(1:2,each=50))
